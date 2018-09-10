3b4c83ccf493a9eda65a0e8ac507097d8ace2ae0
TJ LaGrow <tlagrow@uoregon.edu>
2018-09-10 15:37:06 -0400

    adding patch extraction code

--- patch_extraction/script_run_patch_extraction.m
+++ patch_extraction/script_run_patch_extraction.m
@@ -4,16 +4,15 @@
 % set everything that is hard coded
 patch_size = 150; 
 hyp = 1250;
-%patches_of_interest = [350 650 1400 2500 3500];
-patches_of_interest = [1600 1650 1850 1851];
+patches_of_interest = [350 650 1400 2500 3500];
 
 
 %%  import images and ground truth
-im1 = imread('../data/100048576_293-crop.jpg');
+im1 = imread('100048576_293-crop.jpg');
 im2 = im1(1:3737, 1:4620);
 %large_image = imread('../data/100048576_293.jpg'); %full image
 
-ground_truth = load_nii('../data/100048576_293-crop_y_1151_1350_x_1651_1850_JP_anno.nii');
+ground_truth = load_nii('100048576_293-crop_y_1151_1350_x_1651_1850_JP_anno.nii');
 im_train = ground_truth.img(:,:,2);
 
 %im3 = imread('../data/Nmap_cortex.tiff');
@@ -111,7 +110,6 @@ for m = 1:length(patches_of_interest)
     xlabel(s),
     hold on;
 end
-
 
 
 %EOF
