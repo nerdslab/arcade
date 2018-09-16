ec9a851780eb252446ddf728153fb7f22c10818d
TJ LaGrow <tlagrow@uoregon.edu>
2018-09-16 13:47:47 -0400

    adding flags:

--- cell_detection/script_cell_detection.m
+++ cell_detection/script_cell_detection.m
@@ -9,27 +9,36 @@ Description: This script is an example of the cell detection used in the
 %}
 
 %% load in previous data 
-%load('cell_detection_09_14_2018');
 
 % choose 1 if you want to run the long cell detection test, 0 otherwise
 long_test_flag = 0;
+
+% mark 1 to see only figures, or 0 to run code yourself
+ex_flag = 1;
+
+if ex_flag == 1
+    load('cell_detection_09_14_2018');
+end
+ex_flag = 1;
 
 %% Generate Probability Maps
 
 % Gaussian Mixture Models
 
-numcomp = 2; 
-numsamp = 5e6;
-numfreq = 0; 
-im_og = rgb2gray(im2double(imread('100048576_293_nissl.jpg'))); % V1 image
-im = rgb2gray(im2double(imread('100048576_293_nissl_crop.jpg'))); % V1 image croped
-whichsamp = randi(numel(im),numsamp,1);
-traind = im(whichsamp);
-gm = fitgmdist(traind,numcomp);
-[~,whichCell] = min(gm.mu); 
-Probx = posterior(gm,im(:));
-CellMap = reshape(Probx(:,whichCell),size(im));
-CellMapErode = imerode(CellMap,strel(2));
+if ex_flag == 0
+    numcomp = 2; 
+    numsamp = 5e6;
+    numfreq = 0; 
+    im_og = rgb2gray(im2double(imread('100048576_293_nissl.jpg'))); % V1 image
+    im = rgb2gray(im2double(imread('100048576_293_nissl_crop.jpg'))); % V1 image croped
+    whichsamp = randi(numel(im),numsamp,1);
+    traind = im(whichsamp);
+    gm = fitgmdist(traind,numcomp);
+    [~,whichCell] = min(gm.mu); 
+    Probx = posterior(gm,im(:));
+    CellMap = reshape(Probx(:,whichCell),size(im));
+    CellMapErode = imerode(CellMap,strel(2));
+end
 
 % plot the probability produced
 figure, imshow(CellMapErode), title('GMM Probability Map');
@@ -38,16 +47,18 @@ figure, imshow(CellMapErode), title('GMM Probability Map');
 
 %% Iterative Cell Detection
 
-% load in ground truth
-image = imread('100048576_293_nissl_crop.jpg');
-ground_truth = load_nii('100048576_293_nissl_crop_y_1151_1350_x_1651_1850_JP_anno.nii');
+if ex_flag == 0
+    % load in ground truth
+    image = imread('100048576_293_nissl_crop.jpg');
+    ground_truth = load_nii('100048576_293_nissl_crop_y_1151_1350_x_1651_1850_JP_anno.nii');
 
-row_idx = 1151:1350;
-col_idx = 1651:1850;
+    row_idx = 1151:1350;
+    col_idx = 1651:1850;
 
-im_actual = im(row_idx,col_idx);
-im_test = CellMapErode(row_idx,col_idx);
-im_train = ground_truth.img(:,:,2);
+    im_actual = im(row_idx,col_idx);
+    im_test = CellMapErode(row_idx,col_idx);
+    im_train = ground_truth.img(:,:,2);
+end
 
 % display the testing patch and ground truth
 figure, 
@@ -58,26 +69,28 @@ subplot(1,3,3), imagesc(im_train), title('Ground Truth Annotations')
 
 %% hyperparameter optimization
 
-p_threshold = [0.2:0.1:0.5];
-p_residual = [0.5:0.1:0.7];
-sphere_sz = [6:2:12];
-dilate_sz = 2:1:4;
-max_numcells = 500;
-NumIter = prod([length(p_threshold),length(p_residual),length(sphere_sz),length(dilate_sz)]);
+if ex_flag == 0
+    p_threshold = [0.2:0.1:0.5];
+    p_residual = [0.5:0.1:0.7];
+    sphere_sz = [6:2:12];
+    dilate_sz = 2:1:4;
+    max_numcells = 500;
+    NumIter = prod([length(p_threshold),length(p_residual),length(sphere_sz),length(dilate_sz)]);
+end
 
 %% run the optimization sweep
-Params = hyperparam_celldetect(im_test,im_train,p_threshold,p_residual,sphere_sz,dilate_sz,max_numcells)
-
+if ex_flag == 0
+    Params = hyperparam_celldetect(im_test,im_train,p_threshold,p_residual,sphere_sz,dilate_sz,max_numcells)
+end
 %% Graph estimated cells on cutouts
-
-[Centroids,Nmap] = OMP_ProbMap2D(im_test,Params.p_threshold,Params.p_residual,Params.sphere_sz,Params.dilate_sz,max_numcells,0);
-
-stats = regionprops('table',Nmap,'Centroid',...
-    'MajorAxisLength','MinorAxisLength');
-centers = stats.Centroid;
-diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
-radii = diameters/2; %radius
-
+if ex_flag == 0
+    [Centroids,Nmap] = OMP_ProbMap2D(im_test,Params.p_threshold,Params.p_residual,Params.sphere_sz,Params.dilate_sz,max_numcells,0);
+    stats = regionprops('table',Nmap,'Centroid',...
+        'MajorAxisLength','MinorAxisLength');
+    centers = stats.Centroid;
+    diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+    radii = diameters/2; %radius
+end
 
 %% display the testing patch and ground truth
 figure, 
