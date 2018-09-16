b672cd65e2b6d8b2b10bd2a94826d5792824ce6e
TJ LaGrow <tlagrow@uoregon.edu>
2018-09-16 11:11:52 -0400

    spring cleaning

--- cell_detection/randIntervals.m
+++
@@ -1,8 +0,0 @@
-function [outputArray] = randIntervals(varArray,lengthArray)
-%RANDINTERVALS Summary of this function goes here
-%   Detailed explanation goes here
-
-outputArray = sort((max(varArray)-min(varArray)).*rand(floor(lengthArray),1) + min(varArray))';
-
-end
-
